a1bed8e37b667fbd694f7f93d3fab47a88b88aba
Angel Ejugh <aejugh@gmail.com>
2022-12-07 13:45:56 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+
+    extend: {
+      colors: {
+        "darkblue": "#111827",
+        "whiteback": "#E5E7EB",
+        "purple": "#594ef7",
+        "lightpurple": "#7b74ec",
+        "lightblue": "#5c8af0;",
+        "lightpink": "#e45d99",
+        "greenish": "#74daec",
+        "grey": "#E5E7EB",
+        "dirtywhite": "#f9fafb"
+      }
+    },
+  },
+  plugins: [],
+}
